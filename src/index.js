99b4c4a8afd1770c987a2309d2f9cd56c77e2a6b
L-E-Crane <107731061+L-E-Crane@users.noreply.github.com>
2023-07-06 16:33:28 -0700

    wiped messy code and re-started from scratch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import Weather from "./Weather";
+
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Weather />
+    <App />
   </React.StrictMode>
 );
 
