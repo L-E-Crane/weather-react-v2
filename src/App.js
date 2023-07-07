0e5a1ac52e0f00a7c60370ecf9e9aa3241ce2c5a
L-E-Crane <107731061+L-E-Crane@users.noreply.github.com>
2023-07-06 17:02:57 -0700

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-
+      <button className="btn btn-danger">hello</button>
       <footer>
         <a href="https://github.com/L-E-Crane/weather-react-v2">GitHub Here</a>
       </footer>
